c9898ae11cd7cd11efb6e1de2094edbaea5cfbc9
mshns <mishanos412@gmail.com>
2023-02-01 07:43:48 +0300

    feat: add header, main and footer

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.scss";
+
+import Header from "./components/header/Header";
+import Main from "./components/main/Main";
+import Footer from "./components/footer/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,13 @@
+import "./Header.scss";
+
+function Header() {
+  return (
+    <header className="header">
+      <h1 className="header_logo">
+        Battle<span className="logo-image">Ship</span>
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
