d4e07cc3469b5f874c60e449c921e7bc52c6b8ad
Nikita Kisly <sour945@gmail.com>
2023-02-02 19:20:37 +0300

    fix: update to pnpm

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    open: "http://localhost:5173/",
+  },
   plugins: [react(), svgr()],
 });
