6f3ae784463ee6616d650a2e2ce84b2dd09767f4
mshns <mishanos412@gmail.com>
2023-02-03 06:37:45 +0300

    refactor: add scss colors

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,8 +1,8 @@
 import "./App.scss";
 
-import Header from "./components/header/Header";
-import Main from "./components/main/Main";
-import Footer from "./components/footer/Footer";
+import Header from "../header/Header";
+import Main from "../main/Main";
+import Footer from "../footer/Footer";
 
 function App() {
   return (
