74fb222db3444d0ba268886f2a2f029ec5a2de93
Nikita Kisly <sour945@gmail.com>
2023-02-03 10:23:08 +0300

    feat: add route to login page

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
@@ -8,4 +9,9 @@ export default defineConfig({
     open: "http://localhost:5173/",
   },
   plugins: [react(), svgr()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
 });
