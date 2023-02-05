7491142a0cad74b51b8043838b7c7470e8fe6dd4
Nikita Kisly <sour945@gmail.com>
2023-02-06 01:33:19 +0300

    feat: update config

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,15 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import mkcert from 'vite-plugin-mkcert';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     open: 'http://localhost:5173/',
+    https: true,
   },
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr(), mkcert()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
