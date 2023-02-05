98f11e1a01139c582922dc0ceae0e3e1d4f0e222
Erkhan Denis <erhan.denis@list.ru>
2023-02-05 23:38:44 +0500

    feat: add change ship rotation

--- src/lib/API/DargNDrop.tsx
+++ src/lib/API/DargNDrop.tsx
@@ -26,13 +26,15 @@ export const dragOverHandler = (event: DragEvent<HTMLDivElement>) => {
 export const dragEndHandler = (event: DragEvent<HTMLDivElement>) => {
   const target = event.target as HTMLDivElement;
   const targetId = Number(target.id);
-  const parent = event.target.parentElement;
+  const parent = target.parentElement as HTMLDivElement;
   target.classList.remove('green', 'red');
-  if (parent.childNodes[targetId + 10]) {
-    parent.childNodes[targetId + 10].classList.remove('green', 'red');
+  const neighbor1 = parent.childNodes[targetId + 10] as HTMLDivElement;
+  const neighbor2 = parent.childNodes[targetId + 20] as HTMLDivElement;
+  if (neighbor1) {
+    neighbor1.classList.remove('green', 'red');
   }
-  if (parent.childNodes[targetId + 20]) {
-    parent.childNodes[targetId + 20].classList.remove('green', 'red');
+  if (neighbor2) {
+    neighbor2.classList.remove('green', 'red');
   }
 };
 
--- src/pages/home/components/ship/ship.tsx
+++ src/pages/home/components/ship/ship.tsx
@@ -1,14 +1,24 @@
+import { useEffect, useState } from 'react';
 import './ship.scss';
 
 const Ship = () => {
+  const [horizontal, setHorizonal] = useState(false);
+  const [clickCount, setClickCount] = useState(0);
+
+  const rotateHandler = () => {
+    setClickCount(clickCount + 1);
+    setTimeout(() => {
+      setClickCount(0);
+    }, 400);
+  };
+
+  useEffect(() => {
+    if (clickCount === 2) {
+      setHorizonal(!horizontal);
+    }
+  }, [clickCount]);
   return (
-    <div
-      className="ship"
-      draggable={true}
-      onWheel={() => {
-        console.log('123');
-      }}
-    >
+    <div className={`ship${horizontal ? ' horizontal' : ''}`} draggable={true} onClick={rotateHandler}>
       <div className="cell"></div>
       <div className="cell"></div>
       <div className="cell"></div>
