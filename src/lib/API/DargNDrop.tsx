9aece7a98c613dfecd68b65b2ec5e315747bd554
Erkhan Denis <erhan.denis@list.ru>
2023-02-07 00:46:33 +0500

    feat: add horizontal 4-deck ship

--- src/lib/API/DargNDrop.tsx
+++ src/lib/API/DargNDrop.tsx
@@ -2,6 +2,8 @@ import { DragEvent } from 'react';
 
 export const dragOverHandler = (event: DragEvent<HTMLDivElement>, horizontalRotation: Boolean, shipLength: number) => {
   event.preventDefault();
+  const notAvailableIndexesRight = [9, 19, 29, 39, 49, 59, 69, 79, 89];
+  const notAvailableIndexesLeft = [10, 20, 30, 40, 50, 60, 70, 80, 90];
   const target = event.target as HTMLDivElement;
   const targetId = Number(target.id);
   const parent = event.target.parentElement;
@@ -64,12 +66,16 @@ export const dragOverHandler = (event: DragEvent<HTMLDivElement>, horizontalRota
         }
     }
   } else {
+    const delta1 = 1;
+    const delta2 = -1;
+    const delta3 = -2;
+    const elements = [
+      parent.childNodes[targetId + delta1],
+      parent.childNodes[targetId + delta2],
+      parent.childNodes[targetId + delta3],
+    ];
     switch (shipLength) {
       case 3:
-        const notAvailableIndexesRight = [9, 19, 29, 39, 49, 59, 69, 79, 89];
-        const notAvailableIndexesLeft = [10, 20, 30, 40, 50, 60, 70, 80, 90];
-        const delta1 = 1;
-        const delta2 = -1;
         if (!parent.childNodes[targetId + delta1] && !parent.childNodes[targetId + delta2]) {
           target.classList.add('red');
         }
@@ -106,6 +112,30 @@ export const dragOverHandler = (event: DragEvent<HTMLDivElement>, horizontalRota
           target.classList.add('green');
           parent.childNodes[targetId + delta1].classList.add('green');
           parent.childNodes[targetId + delta2].classList.add('green');
+        }
+      case 4:
+        if (notAvailableIndexesRight.includes(targetId)) {
+          target.classList.add('red');
+          elements.filter((element, index) => element && index > 0).forEach((element) => element.classList.add('red'));
+        }
+        if (notAvailableIndexesLeft.includes(targetId) || notAvailableIndexesLeft.includes(targetId - 1)) {
+          target.classList.add('red');
+          elements
+            .filter((element) => Number(element.id) >= Math.floor(targetId / 10) * 10)
+            .forEach((element) => element.classList.add('red'));
+        }
+        if (!elements.every((element) => element)) {
+          target.classList.add('red');
+          elements.filter((element) => element).forEach((element) => element.classList.add('red'));
+        }
+        if (
+          elements.every((element) => element) &&
+          !notAvailableIndexesRight.includes(Number(target.id)) &&
+          !notAvailableIndexesLeft.includes(Number(target.id)) &&
+          !notAvailableIndexesLeft.includes(Number(target.id) - 1)
+        ) {
+          target.classList.add('green');
+          elements.forEach((element) => element.classList.add('green'));
         }
     }
   }
@@ -188,10 +218,10 @@ export const dropHadler = (event: DragEvent<HTMLDivElement>, horizontalRotation:
         }
     }
   } else {
+    const notAvailableIndexesRight = [9, 19, 29, 39, 49, 59, 69, 79, 89];
+    const notAvailableIndexesLeft = [10, 20, 30, 40, 50, 60, 70, 80, 90];
     switch (shipLength) {
       case 3:
-        const notAvailableIndexesRight = [9, 19, 29, 39, 49, 59, 69, 79, 89];
-        const notAvailableIndexesLeft = [10, 20, 30, 40, 50, 60, 70, 80, 90];
         if (notAvailableIndexesRight.includes(targetId) || notAvailableIndexesLeft.includes(targetId)) {
           children[targetId].classList.remove('red');
           children[targetId + 1].classList.remove('red');
@@ -208,6 +238,36 @@ export const dropHadler = (event: DragEvent<HTMLDivElement>, horizontalRotation:
           }
           if (parent.childNodes[targetId - 1]) {
             parent.childNodes[targetId - 1].classList.remove('green', 'red');
+          }
+          return;
+        }
+      case 4:
+        if (notAvailableIndexesRight.includes(targetId) || notAvailableIndexesLeft.includes(targetId)) {
+          children[targetId].classList.remove('red');
+          children[targetId + 1].classList.remove('red');
+          children[targetId - 1].classList.remove('red');
+          children[targetId - 2].classList.remove('red');
+          return;
+        } else if (
+          children[targetId + 1] &&
+          children[targetId - 1] &&
+          children[targetId - 2] &&
+          Math.floor((targetId - 2) / 10) === Math.floor(targetId / 10)
+        ) {
+          children[targetId].classList.add('ship-1');
+          children[targetId + 1].classList.add('ship-1');
+          children[targetId - 1].classList.add('ship-1');
+          children[targetId - 2].classList.add('ship-1');
+        } else {
+          target.classList.remove('green', 'red');
+          if (parent.childNodes[targetId + 1]) {
+            parent.childNodes[targetId + 1].classList.remove('green', 'red');
+          }
+          if (parent.childNodes[targetId - 1]) {
+            parent.childNodes[targetId - 1].classList.remove('green', 'red');
+          }
+          if (parent.childNodes[targetId - 2]) {
+            parent.childNodes[targetId - 2].classList.remove('green', 'red');
           }
           return;
         }
