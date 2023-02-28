cfc89d985ba62c7dd85d00c2396379d0ff476f07
Nikita Kisly <sour945@gmail.com>
2023-02-28 11:19:12 +0300

    feat: add chat sound

--- src/hook/socketHandlers/use-chat.ts
+++ src/hook/socketHandlers/use-chat.ts
@@ -1,27 +1,32 @@
 import { useAppSelector, useChatActions } from '@/hook/_index';
 import { IMessage } from '@/store/reducers/types/socket';
 import { CHAT } from '@/store/_constants';
+import Sound from '@/lib/API/Sound/Sound';
 
 export const useChatHandler = () => {
   const { pushGameMessage, pushCommonMessage, setUnreadCommon, setUnreadGame } =
     useChatActions();
-  const { gameInfo, userName } = useAppSelector((state) => {
+  const { gameInfo, userName, sound } = useAppSelector((state) => {
     const { gameInfo } = state.gameStateSlice;
     const { userName } = state.logInSlice;
+    const { sound } = state.appSettingsSlice;
 
-    return { gameInfo, userName };
+    return { gameInfo, userName, sound };
   });
 
   const chatHandler = (data: IMessage) => {
     if (data.mail.chatName === CHAT.game) {
       if (gameInfo?.gameId === data.mail.gameId) {
-        pushGameMessage(data.mail);
+        if (sound) Sound('chat');
 
+        pushGameMessage(data.mail);
         if (userName !== data.mail.name) setUnreadGame();
       }
     }
 
     if (data.mail.chatName === CHAT.common) {
+      if (sound) Sound('chat');
+
       pushCommonMessage(data.mail);
       if (userName !== data.mail.name) setUnreadCommon();
     }
