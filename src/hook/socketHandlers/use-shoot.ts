bb81aa1d56e79748ed692aa8b1603785dd71ea86
Nikita Kisly <sour945@gmail.com>
2023-03-01 00:42:20 +0300

    refactor: change not allowed cells handler to useEffect

--- src/hook/socketHandlers/use-shoot.ts
+++ src/hook/socketHandlers/use-shoot.ts
@@ -1,3 +1,5 @@
+// import { useEffect, useState } from 'react';
+
 import {
   useAppSelector,
   useGameStateActions,
@@ -16,27 +18,38 @@ export const useShootHandler = () => {
   // const userState = useAppSelector((state) => state.gameShipsSlice.user);
   // const rivalState = useAppSelector((state) => state.gameShipsSlice.rival);
 
+  // const [stopUseEffect, setStopUseEffect] = useState(false);
+
+  // useEffect(() => {
+  //   console.log('useEffect', stopUseEffect);
+  //   if (!stopUseEffect) {
+  //     userState.ships.forEach(async (ship) => {
+  //       if (ship.decks === ship.woundedCells.length) {
+  //         await setStopUseEffect(true);
+  //         addNotAllowed(PERSON.user, ship.occupiedCells, ship.decks);
+  //         await setStopUseEffect(false);
+  //       }
+  //     });
+
+  //     rivalState.ships.forEach(async (ship) => {
+  //       if (ship.decks === ship.woundedCells.length) {
+  //         await setStopUseEffect(true);
+  //         addNotAllowed(PERSON.rival, ship.occupiedCells, ship.decks);
+  //         await setStopUseEffect(false);
+  //       }
+  //     });
+  //   }
+  // }, [userState.ships, rivalState.ships]);
+
   const shootHandler = (data: IShoot) => {
     const { user, shoot, isAbleShoot } = data;
 
     if (user === userName) {
       setIsAbleShoot(isAbleShoot);
       checkShoot(PERSON.rival, shoot);
-
-      // userState.ships.forEach((ship) => {
-      //   if (ship.decks === ship.woundedCells.length) {
-      //     addNotAllowed(PERSON.user, ship.occupiedCells, ship.decks);
-      //   }
-      // });
     } else {
       setIsAbleShoot(!isAbleShoot);
       checkShoot(PERSON.user, shoot);
-
-      // rivalState.ships.forEach((ship) => {
-      //   if (ship.decks === ship.woundedCells.length) {
-      //     addNotAllowed(PERSON.rival, ship.occupiedCells, ship.decks);
-      //   }
-      // });
     }
     console.log('shoot');
   };
