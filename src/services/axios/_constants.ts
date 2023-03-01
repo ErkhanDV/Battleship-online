ad4f28a8a205c77a6eaae7bfe91a3caf4810c6aa
Nikita Kisly <sour945@gmail.com>
2023-03-01 12:25:34 +0300

    fix: switch server

--- src/services/axios/_constants.ts
+++ src/services/axios/_constants.ts
@@ -1,10 +1,10 @@
-export const CLONE_SERVER = 'http://localhost:5000/api';
+// export const CLONE_SERVER = 'http://localhost:5000/api';
 
-export const SOCKET = 'ws://localhost:5000/game';
+// export const SOCKET = 'ws://localhost:5000/game';
 
-// export const CLONE_SERVER = 'https://battle-ship.up.railway.app/api';
+export const CLONE_SERVER = 'https://battle-ship.up.railway.app/api';
 
-// export const SOCKET = 'wss://battle-ship.up.railway.app/game';
+export const SOCKET = 'wss://battle-ship.up.railway.app/game';
 
 export enum SOCKETMETHOD {
   connect = 'connection',
